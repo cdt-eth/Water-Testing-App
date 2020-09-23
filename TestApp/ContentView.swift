f106b970091f40f5ef5791bf9c1ecb5bccad493a
christian <christiandavidphoto@gmail.com>
2020-09-23 14:50:46 -0400

    phase 1

--- TestApp/ContentView.swift
+++ TestApp/ContentView.swift
@@ -9,8 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(alignment: .leading) {
+            Text("Water Testing App").font(.title)
+            HStack {
+                Text("BioLab Inc.").font(.subheadline)
+                Spacer()
+                Text("(C) 2020").font(.subheadline)
+            }
+        }.padding()
     }
 }
 
--- TestApp/TestAppApp.swift
+++ TestApp/TestAppApp.swift
@@ -15,3 +15,9 @@ struct TestAppApp: App {
         }
     }
 }
+
+struct TestAppApp_Previews: PreviewProvider {
+    static var previews: some View {
+        /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/
+    }
+}
