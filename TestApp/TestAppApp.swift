d872d878e802ac8e0baa58e1f174792d60c61ae0
christian <christiandavidphoto@gmail.com>
2020-09-23 10:31:53 -0400

    first commit

---
+++ TestApp/TestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppApp.swift
+//  TestApp
+//
+//  Created by Christian David Turner on 9/23/20.
+//
+
+import SwiftUI
+
+@main
+struct TestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
