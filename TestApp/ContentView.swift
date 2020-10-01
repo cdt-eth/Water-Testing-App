6e3903864d852b6a1b32c073b4ae0a7fae148988
christian <christiandavidphoto@gmail.com>
2020-10-01 14:37:50 -0400

    progress on toggle menu

--- TestApp/ContentView.swift
+++ TestApp/ContentView.swift
@@ -9,19 +9,70 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("Water Testing App").font(.title)
-            HStack {
-                Text("BioLab Inc.").font(.subheadline)
-                Spacer()
-                Text("(C) 2020").font(.subheadline)
-            }
-        }.padding()
+        Home()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct Home: View {
+    var body: some View {
+        ZStack(alignment: .leading){
+            GeometryReader{_ in
+                VStack{
+                    Text("Dark Mode Menu")
+                }
+            }
+            Menu()
+        }
+    }
+}
+
+
+struct  Menu: View {
+    var body: some View {
+        VStack{
+            HStack{
+                Button(action: {
+                    
+                }) {
+                    Image("back")
+                        .resizable()
+                        .frame(width:12, height: 20)
+                }
+                Spacer()
+                
+                Button(action: {
+                    
+                }) {
+                    Image(systemName: "square.and.pencil")
+                        .font(.title)
+                }
+            }
+            .padding(.top)
+            .padding(.bottom, 25)
+            
+            Image("pic")
+                .resizable()
+                .frame(width: 80, height: 80)
+                .clipShape(Circle())
+            
+            VStack(spacing: 12){
+                
+                Text("Christian")
+                
+                Text("Developer")
+                    .font(.caption)
+            }
+            .padding(.top, 25)
+            
+            Spacer()
+        }
+        .frame(width: UIScreen.main.bounds.width / 1.5)
+        .padding(.horizontal, 20)
     }
 }
