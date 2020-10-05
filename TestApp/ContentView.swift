049c3ed8fe6b747e73df8e1bb62fce1196dd810d
christian <christiandavidphoto@gmail.com>
2020-10-05 10:03:47 -0400

    isShowingDetailView button

--- TestApp/ContentView.swift
+++ TestApp/ContentView.swift
@@ -17,193 +17,205 @@ struct ResultView: View {
 
 
 struct ContentView: View {
+    @State private  var isShowingDetailView = false
+    
     var body: some View {
         
         NavigationView{
             VStack(spacing: 30){
-                Text("Flip a coin. Do you want heads or tails?")
-                NavigationLink(destination: ResultView(choice: "Heads")) {
-                    Text("Choose Heads")
+                NavigationLink(destination: Text("Second View"), isActive: $isShowingDetailView) { EmptyView() }
+//                    Text("Choose Heads")
+//                }
+                
+                // USE THIS, but how do i set destination as a specific page?
+                Button("Tap to show detial"){
+                    isShowingDetailView = true
                 }
-                NavigationLink(destination: ResultView(choice: "Tails")) {
-                    Text("Choose Tails")
+                    
+                    
+                    //                Text("Flip a coin. Do you want heads or tails?")
+                    //                NavigationLink(destination: ResultView(choice: "Heads")) {
+                    //                    Text("Choose Heads")
+                    //                }
+                    //                NavigationLink(destination: ResultView(choice: "Tails")) {
+                    //                    Text("Choose Tails")
+                    //                }
                 }
+                .navigationTitle("Navigation")
+                
+                //            NavigationLink(destination: Text("Made it!")) {
+                //                Text("First Link")
+                //                Image("BioLab")
+                //                    .renderingMode(.original)
+                //                    .resizable()
+                //                    .frame(width: 140, height: 60)
+                //            }
+                //            Text("Home")
+                //                .navigationTitle("Navigation")
+                
             }
-            .navigationTitle("Navigation")
-            
-//            NavigationLink(destination: Text("Made it!")) {
-//                Text("First Link")
-//                Image("BioLab")
-//                    .renderingMode(.original)
-//                    .resizable()
-//                    .frame(width: 140, height: 60)
-//            }
-//            Text("Home")
-//                .navigationTitle("Navigation")
-            
         }
     }
-}
-
-//struct ContentView: View {
-//    var body: some View {
-//
-//        Home()
-//    }
-//}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    //struct ContentView: View {
+    //    var body: some View {
+    //
+    //        Home()
+    //    }
+    //}
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
-}
-
-struct Home: View {
     
-    @State var dark = false
-    @State var show = false
-    
-    var body: some View {
-        ZStack(alignment: .leading){
-            GeometryReader{_ in
-                VStack{
-                    ZStack{
-                        HStack{
-                            Button(action: {
-                                withAnimation(.default){
-                                    show.toggle()
+    struct Home: View {
+        
+        @State var dark = false
+        @State var show = false
+        
+        var body: some View {
+            ZStack(alignment: .leading){
+                GeometryReader{_ in
+                    VStack{
+                        ZStack{
+                            HStack{
+                                Button(action: {
+                                    withAnimation(.default){
+                                        show.toggle()
+                                    }
+                                }) {
+                                    Image(systemName: "line.horizontal.3")
+                                        .font(.title)
                                 }
-                            }) {
-                                Image(systemName: "line.horizontal.3")
+                                Spacer()
+                                
+                                Image("BioLab")
+                                    .resizable()
+                                    .frame(width: 140, height: 60)
+                                
+                                Spacer()
+                                
+                                Image(systemName: "person")
                                     .font(.title)
                             }
-                            Spacer()
-                            
-                            Image("BioLab")
-                                .resizable()
-                                .frame(width: 140, height: 60)
-                            
-                            Spacer()
-                            
-                            Image(systemName: "person")
-                                .font(.title)
                         }
-                    }
-                    .padding()
-                    .foregroundColor(.primary)
-                    .overlay(Rectangle().stroke(Color.primary.opacity(0.1), lineWidth: 1)
-                                .shadow(radius:1).edgesIgnoringSafeArea(.all))
-                    
-                    Spacer()
-                    
-                    Image("home-pool")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .padding(.bottom, 35)
-                    
-                    
-                    
-                    Text("Test your pool water for immediate and accurate product and dosage recommendations.")
-                        .multilineTextAlignment(.center)
-                        .frame(width: 350)
-                    
-                    Spacer()
-                    
-                    Button(action: {
+                        .padding()
+                        .foregroundColor(.primary)
+                        .overlay(Rectangle().stroke(Color.primary.opacity(0.1), lineWidth: 1)
+                                    .shadow(radius:1).edgesIgnoringSafeArea(.all))
                         
-                    }) {
-                        HStack {
-                            Image(systemName: "eyedropper.halffull")
-                                .font(.title)
-                                .foregroundColor(.white)
-                            //                                .padding()
-                            Text("TEST POOL WATER NOW")
-                                .font(.callout)
-                                .fontWeight(.bold)
-                                .foregroundColor(.white)
-                                .padding()
+                        Spacer()
+                        
+                        Image("home-pool")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .padding(.bottom, 35)
+                        
+                        
+                        
+                        Text("Test your pool water for immediate and accurate product and dosage recommendations.")
+                            .multilineTextAlignment(.center)
+                            .frame(width: 350)
+                        
+                        Spacer()
+                        
+                        Button(action: {
+                            
+                        }) {
+                            HStack {
+                                Image(systemName: "eyedropper.halffull")
+                                    .font(.title)
+                                    .foregroundColor(.white)
+                                //                                .padding()
+                                Text("TEST POOL WATER NOW")
+                                    .font(.callout)
+                                    .fontWeight(.bold)
+                                    .foregroundColor(.white)
+                                    .padding()
+                            }
+                            .padding(.horizontal, 20)
+                            .background(Color("DarkBlue"))
+                            .cornerRadius(10)
+                            .frame(maxWidth: .infinity)
+                            //                        .padding(.top, 50)
+                            
                         }
-                        .padding(.horizontal, 20)
-                        .background(Color("DarkBlue"))
-                        .cornerRadius(10)
-                        .frame(maxWidth: .infinity)
-                        //                        .padding(.top, 50)
                         
+                        
+                        Spacer()
                     }
-                    
-                    
-                    Spacer()
                 }
-            }
-            
-            HStack {
-                Menu(dark: $dark, show: $show)
-                    .preferredColorScheme(dark ? .dark : .light)
-                    .offset(x: show ? 0 : -UIScreen.main.bounds.width / 1.5)
                 
-                Spacer(minLength: 0)
-            }
-            .background(Color.primary.opacity(show ? (dark ? 0.05 : 0.2) : 0).edgesIgnoringSafeArea(.all))
-        }
-    }
-}
-
-
-
-struct  Menu: View {
-    
-    @Binding var dark: Bool
-    @Binding var show: Bool
-    
-    var body: some View {
-        
-        VStack{
-            HStack{
-                Button(action: {
-                    withAnimation(.default){
-                        show.toggle()
-                    }
-                }) {
-                    Image(systemName: "chevron.left")
-                        .font(.title)
+                HStack {
+                    Menu(dark: $dark, show: $show)
+                        .preferredColorScheme(dark ? .dark : .light)
+                        .offset(x: show ? 0 : -UIScreen.main.bounds.width / 1.5)
+                    
+                    Spacer(minLength: 0)
                 }
-
-                Spacer()
-//
-//                Button(action: {
-//
-//                }) {
-//                    Image(systemName: "eyedropper.halffull")
-//                        .font(.title)
-//                }
+                .background(Color.primary.opacity(show ? (dark ? 0.05 : 0.2) : 0).edgesIgnoringSafeArea(.all))
             }
-            .padding(.top)
-            .padding(.bottom, 25)
-            
-            
-            Image("BioLab")
-                .resizable()
-                .frame(width: 140, height: 60)
-            
-//            VStack(spacing: 12){
-//                Text("Water Testing App")
-//                Text("© 2020").font(.caption)
-//            }.padding(.top, 15)
-            
-            Divider()
-                .padding()
-            
-            Navigation()
-            
-            Spacer()
         }
-        .foregroundColor(.primary)
-        .padding(.horizontal, 20)
-        .frame(width: UIScreen.main.bounds.width / 1.5)
-        .background((dark ? Color.black : Color.white).edgesIgnoringSafeArea(.all))
-        .overlay(Rectangle().stroke(Color.primary.opacity(0.2), lineWidth: 2)
-                    .shadow(radius:3 )
-                    .edgesIgnoringSafeArea(.all)
-        )
     }
-}
+    
+    
+    
+    struct  Menu: View {
+        
+        @Binding var dark: Bool
+        @Binding var show: Bool
+        
+        var body: some View {
+            
+            VStack{
+                HStack{
+                    Button(action: {
+                        withAnimation(.default){
+                            show.toggle()
+                        }
+                    }) {
+                        Image(systemName: "chevron.left")
+                            .font(.title)
+                    }
+                    
+                    Spacer()
+                    //
+                    //                Button(action: {
+                    //
+                    //                }) {
+                    //                    Image(systemName: "eyedropper.halffull")
+                    //                        .font(.title)
+                    //                }
+                }
+                .padding(.top)
+                .padding(.bottom, 25)
+                
+                
+                Image("BioLab")
+                    .resizable()
+                    .frame(width: 140, height: 60)
+                
+                //            VStack(spacing: 12){
+                //                Text("Water Testing App")
+                //                Text("© 2020").font(.caption)
+                //            }.padding(.top, 15)
+                
+                Divider()
+                    .padding()
+                
+                Navigation()
+                
+                Spacer()
+            }
+            .foregroundColor(.primary)
+            .padding(.horizontal, 20)
+            .frame(width: UIScreen.main.bounds.width / 1.5)
+            .background((dark ? Color.black : Color.white).edgesIgnoringSafeArea(.all))
+            .overlay(Rectangle().stroke(Color.primary.opacity(0.2), lineWidth: 2)
+                        .shadow(radius:3 )
+                        .edgesIgnoringSafeArea(.all)
+            )
+        }
+    }
