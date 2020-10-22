aaf1f24092fef8258adb7fefd633390f89b32ff5
christian <christiandavidphoto@gmail.com>
2020-10-22 13:56:19 -0400

    added customer view and added site linkout

---
+++ TestApp/CustomerView.swift
@@ -0,0 +1,20 @@
+//
+//  CustomerView.swift
+//  Water Testing
+//
+//  Created by Christian David Turner on 10/22/20.
+//
+
+import SwiftUI
+
+struct CustomerView: View {
+    var body: some View {
+        Text("CUSTOMER VIEW")
+    }
+}
+
+struct CustomerView_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomerView()
+    }
+}
