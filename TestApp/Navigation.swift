5d662fb924c9c8a8349fdf5f2072f7bd1d3ed97d
christian <christiandavidphoto@gmail.com>
2020-10-25 00:03:37 -0400

    clean up nav

--- TestApp/Navigation.swift
+++ TestApp/Navigation.swift
@@ -39,12 +39,6 @@ struct Navigation: View {
                     }
                 }.padding(.top, 25)
                 
-                //                NavigationLink(destination: WaterTestView()) {
-                //                    HStack(spacing: 22) {
-                //                        Text("Website")
-                //                        Spacer()
-                //                    }
-                //                }.padding(.top, 25)
                 
                 HStack(spacing: 22) {
                     Link("Website", destination: URL(string: "https://www.proseriespool.com")!)
@@ -59,88 +53,9 @@ struct Navigation: View {
                     }
                 }.padding(.top, 25)
                 
-                //                Spacer()
                 
             }.navigationBarTitle("")
             .navigationBarHidden(true)
-            
-            
-            //                    Group {
-            ////             MY CUSTOMERS
-            //                        Button(action: {
-            //
-            //                        }) {
-            //                            HStack(spacing: 22) {
-            //
-            //                                Text("My Customers")
-            //                                Spacer()
-            //                            }
-            //                        }
-            //                        .padding(.top, 25)
-            //
-            //
-            //                        // WATER TEST
-            //                        Button(action: {
-            //
-            //                        }) {
-            //                            HStack(spacing: 22) {
-            //                                Text("Water Test")
-            //                                Spacer()
-            //                            }
-            //                        }
-            //                        .padding(.top, 25)
-            //
-            //
-            //                        // VOLUME CALCULATOR
-            //                        Button(action: {
-            //
-            //                        }) {
-            //                            HStack(spacing: 22) {
-            //                                Text("Volume Calculator")
-            //                                Spacer()
-            //                            }
-            //                        }
-            //                        .padding(.top, 25)
-            //
-            //
-            //
-            //                        // CONTACT US
-            //                        Button(action: {
-            //
-            //                        }) {
-            //                            HStack(spacing: 22) {
-            //                                Text("Contact Us")
-            //                                Spacer()
-            //                            }
-            //                        }
-            //                        .padding(.top, 25)
-            //
-            //
-            //
-            //                        // WEBSITE
-            //                        Button(action: {
-            //
-            //                        }) {
-            //                            HStack(spacing: 22) {
-            //                                Text("Website")
-            //                                Spacer()
-            //                            }
-            //                        }
-            //                        .padding(.top, 25)
-            //
-            //
-            //                        // LOGOUT
-            //                        Button(action: {
-            ////                            WaterTestView()
-            //                        }) {
-            //                            HStack(spacing: 22) {
-            //                                Text("Logout")
-            //                                Spacer()
-            //                            }
-            //                        }
-            //                        .padding(.top, 25)
-            //
-            //        }
         }.padding(.bottom,300)
     }
 }
