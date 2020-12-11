2841f6072b60750b03a64d82883f2bcaa1a09281
christian <christiandavidphoto@gmail.com>
2020-12-11 09:21:42 -0500

    code cleanup

--- TestApp/WaterTestView.swift
+++ TestApp/WaterTestView.swift
@@ -29,7 +29,7 @@ struct Reading: Decodable {
 }
 
 
-var results = Welcome()
+//var results = Welcome()
 
 struct WaterTestView: View {
     @State var sliderValue = 0.0
@@ -66,128 +66,128 @@ struct WaterTestView: View {
     var minST = 0.0
     var maxST = 1000.0
     
-//    @State private var results = [Result]()
-    
+    // MARK: - Results from Private Fetch
     @State var res = Welcome()
+    
     var body: some View {
-
-          VStack(alignment: .leading) {
-
+        
+        VStack(alignment: .leading) {
+            
             Text(res.created_at ?? "N/A")
                 .font(.subheadline)
                 .padding()
-            }.onAppear(){
-                fetchResults().getData {(res) in
-                    self.res = res
-                }
+        }.onAppear(){
+            fetchResults().getData {(res) in
+                self.res = res
             }
+        }
         
-    
+        
+        
         VStack(spacing: 0) {
-        
-        Text("Total Hardness").font(.footnote).bold()
-        if valueTH < 40 || valueTH > 750 {
-        Text("\(Int(valueTH))").font(.title).bold().foregroundColor(Color.red)
-        } else  {
-        Text("\(Int(valueTH))").font(.title).bold()
-        }
-        
-        HStack {
-        Text("\(Int(minTH))")
-        
-        Slider(value: $valueTH, in: minTH...maxTH)
-        
-        Text("\(Int(maxTH))")
-        }.padding(EdgeInsets(top: 0, leading: 20, bottom: 20, trailing: 20))
-//        }.onAppear(perform: loadData)
-        }
-        VStack(spacing: 0) {
-        Text("Total Chlorine").font(.footnote).bold()
-        
-        if valueTC < 4 || valueTC > 15 {
-        Text("\(Int(valueTC))").font(.title).bold().foregroundColor(Color.red)
-        } else  {
-        Text("\(Int(valueTC))").font(.title).bold()
-        }
-        
-        HStack {
-        Text("\(Int(minTC))")
-        
-        Slider(value: $valueTC, in: minTC...maxTC)
-        
-        
-        
-        Text("\(Int(maxTC))")
-        }.padding(EdgeInsets(top: 0, leading: 20, bottom: 20, trailing: 20))
+            
+            Text("Total Hardness").font(.footnote).bold()
+            if valueTH < 40 || valueTH > 750 {
+                Text("\(Int(valueTH))").font(.title).bold().foregroundColor(Color.red)
+            } else  {
+                Text("\(Int(valueTH))").font(.title).bold()
+            }
+            
+            HStack {
+                Text("\(Int(minTH))")
+                
+                Slider(value: $valueTH, in: minTH...maxTH)
+                
+                Text("\(Int(maxTH))")
+            }.padding(EdgeInsets(top: 0, leading: 20, bottom: 20, trailing: 20))
         }
         VStack(spacing: 0) {
-        Text("Free Chlorine").font(.footnote).bold()
-        
-        if valueFC < 4 || valueFC > 15 {
-        Text("\(Double(valueFC), specifier: "%.1f")").font(.title).bold().foregroundColor(Color.red)
-        } else  {
-        Text("\(Double(valueFC), specifier: "%.1f")").font(.title).bold()
-        }
-        
-        HStack {
-        Text("\(Int(minFC))")
-        
-        Slider(value: $valueFC, in: minFC...maxFC, step: 0.1)
-        
-        Text("\(Int(maxFC))")
-        }.padding(EdgeInsets(top: 0, leading: 20, bottom: 20, trailing: 20))
-        }
-        VStack(spacing: 0) {
-        Text("pH").font(.footnote).bold()
-        
-        if valuePH < 7.3 || valuePH > 8.0 {
-        Text("\(Double(valuePH), specifier: "%.1f")").font(.title).bold().foregroundColor(Color.red)
-        } else  {
-        Text("\(Double(valuePH), specifier: "%.1f")").font(.title).bold()
-        }
-        
-        HStack {
-        Text("\(Int(minPH))")
-        
-        Slider(value: $valuePH, in: minPH...maxPH, step: 0.1)
-        
-        Text("\(Int(maxPH))")
-        }.padding(EdgeInsets(top: 0, leading: 20, bottom: 20, trailing: 20))
+            Text("Total Chlorine").font(.footnote).bold()
+            
+            if valueTC < 4 || valueTC > 15 {
+                Text("\(Int(valueTC))").font(.title).bold().foregroundColor(Color.red)
+            } else  {
+                Text("\(Int(valueTC))").font(.title).bold()
+            }
+            
+            HStack {
+                Text("\(Int(minTC))")
+                
+                Slider(value: $valueTC, in: minTC...maxTC)
+                
+                
+                
+                Text("\(Int(maxTC))")
+            }.padding(EdgeInsets(top: 0, leading: 20, bottom: 20, trailing: 20))
         }
         VStack(spacing: 0) {
-        
-        Text("Total Alkalinity").font(.footnote).bold()
-        
-        if valueTA < 40 || valueTA > 750 {
-        Text("\(Int(valueTA))").font(.title).bold().foregroundColor(Color.red)
-        } else  {
-        Text("\(Int(valueTA))").font(.title).bold()
-        }
-        
-        HStack {
-        Text("\(Int(minTA))")
-        
-        Slider(value: $valueTA, in: minTA...maxTA)
-        
-        Text("\(Int(maxTA))")
-        }.padding(EdgeInsets(top: 0, leading: 20, bottom: 20, trailing: 20))
+            Text("Free Chlorine").font(.footnote).bold()
+            
+            if valueFC < 4 || valueFC > 15 {
+                Text("\(Double(valueFC), specifier: "%.1f")").font(.title).bold().foregroundColor(Color.red)
+            } else  {
+                Text("\(Double(valueFC), specifier: "%.1f")").font(.title).bold()
+            }
+            
+            HStack {
+                Text("\(Int(minFC))")
+                
+                Slider(value: $valueFC, in: minFC...maxFC, step: 0.1)
+                
+                Text("\(Int(maxFC))")
+            }.padding(EdgeInsets(top: 0, leading: 20, bottom: 20, trailing: 20))
         }
         VStack(spacing: 0) {
-        Text("Stabilizer (CYA)").font(.footnote).bold()
-        
-        if valueST < 40 || valueST > 750 {
-        Text("\(Int(valueST))").font(.title).bold().foregroundColor(Color.red)
-        } else  {
-        Text("\(Int(valueST))").font(.title).bold()
+            Text("pH").font(.footnote).bold()
+            
+            if valuePH < 7.3 || valuePH > 8.0 {
+                Text("\(Double(valuePH), specifier: "%.1f")").font(.title).bold().foregroundColor(Color.red)
+            } else  {
+                Text("\(Double(valuePH), specifier: "%.1f")").font(.title).bold()
+            }
+            
+            HStack {
+                Text("\(Int(minPH))")
+                
+                Slider(value: $valuePH, in: minPH...maxPH, step: 0.1)
+                
+                Text("\(Int(maxPH))")
+            }.padding(EdgeInsets(top: 0, leading: 20, bottom: 20, trailing: 20))
         }
-        
-        HStack {
-        Text("\(Int(minST))")
-        
-        Slider(value: $valueST, in: minST...maxST)
-        
-        Text("\(Int(maxST))")
-        }.padding(EdgeInsets(top: 0, leading: 20, bottom: 20, trailing: 20))
+        VStack(spacing: 0) {
+            
+            Text("Total Alkalinity").font(.footnote).bold()
+            
+            if valueTA < 40 || valueTA > 750 {
+                Text("\(Int(valueTA))").font(.title).bold().foregroundColor(Color.red)
+            } else  {
+                Text("\(Int(valueTA))").font(.title).bold()
+            }
+            
+            HStack {
+                Text("\(Int(minTA))")
+                
+                Slider(value: $valueTA, in: minTA...maxTA)
+                
+                Text("\(Int(maxTA))")
+            }.padding(EdgeInsets(top: 0, leading: 20, bottom: 20, trailing: 20))
+        }
+        VStack(spacing: 0) {
+            Text("Stabilizer (CYA)").font(.footnote).bold()
+            
+            if valueST < 40 || valueST > 750 {
+                Text("\(Int(valueST))").font(.title).bold().foregroundColor(Color.red)
+            } else  {
+                Text("\(Int(valueST))").font(.title).bold()
+            }
+            
+            HStack {
+                Text("\(Int(minST))")
+                
+                Slider(value: $valueST, in: minST...maxST)
+                
+                Text("\(Int(maxST))")
+            }.padding(EdgeInsets(top: 0, leading: 20, bottom: 20, trailing: 20))
         }  .navigationBarTitle(Text("Pool Water Test"), displayMode:.inline)
         
     }
@@ -221,194 +221,12 @@ class fetchResults{
             print("SUCCESS: Got data - \(data! )")
             
             DispatchQueue.main.async {
-                completion(resultList)           // << here !!
+                completion(resultList)   
             }
         }
         task.resume()
     }
-    
 }
-    
-    
-//    func loadData() {
-//        print("Fetch")
-//        let semaphore = DispatchSemaphore (value: 0)
-//        let parameters = "{\n\n  \"pool_volume\": 10000,\n\n  \"pool_product_sanitizer\": 652,\n\n  \"readings\": [\n\n    {\n\n      \"analyte\": \"fc\",\n\n      \"value\": 1.6\n\n    },\n\n    {\n\n      \"analyte\": \"ph\",\n\n      \"value\": 8.6\n\n    }\n\n  ],\n\n  \"include_maintenance_recommendations\": true\n\n}"
-//        let postData = parameters.data(using: .utf8)
-//
-//        var request = URLRequest(url: URL(string: "https://www.biolabhydra.com/api/v3/water_tests")!,timeoutInterval: Double.infinity)
-//        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-//        request.addValue("Basic Y2hyaXN0aWFuLnR1cm5lckBiaW9sYWJpbmMuY29tfDg6RTNYZFlQbnVXZ2hiOTZk", forHTTPHeaderField: "Authorization")
-//        request.addValue("csrftoken=J7rRhxMbgGc5ugfEj2EtwsDV8qo6ZsE7pCqm3N9hutujQs0JSaSbP8sz5sL762ML", forHTTPHeaderField: "Cookie")
-//        request.httpMethod = "POST"
-//        request.httpBody = postData
-//
-//        URLSession.shared.dataTask(with: request) { data, response, error in
-//            guard let data = data else {
-//                print(String(describing: error))
-//                return
-//            }
-////            let results = try! JSONDecoder().decode([Result].self, from: data!)
-////            DispatchQueue.main.async{
-////                completion(results)
-////            }
-//            print(String(data: data, encoding: .utf8)!)
-//            semaphore.signal()
-//        }.resume()
-//        semaphore.wait()
-//    }
-
-    //    -------  -------  -------  -------  -------  -------  -------  --------------
-//    func getProducts(completion: @escaping ([Product]) -> ()) {
-//        guard let url = URL(string: "https://www.biolabhydra.com/api/v3/products") else {return}
-//        URLSession.shared.dataTask(with: url) { (data, _, _) in
-//            let product = try! JSONDecoder().decode([Product].self, from: data!)
-//            DispatchQueue.main.async{
-//                completion(product)
-//            }
-//        }
-//        .resume()
-//    }
-//    -------  -------  -------  -------  -------  -------  -------  --------------
-    //        var body: some View {
-    //            Text("hi")
-    //
-    //            List(results){ result in
-    //
-    //                VStack(alignment: .leading){
-    //    //                Text(result.recommendations ?? "N/A").font(.callout).italic()
-    //    //                Text(result.created_at).font(.callout).italic()
-    //    //                Text(result.pool_system_type).font(.callout).italic()
-    //                    Text("volume: \(result.pool_volume)" )
-    //
-    //                }
-    //
-    //            }
-    //            .onAppear(){
-    //                API().getResults { (results) in
-    //                    self.results = results
-    //                }
-    //            }
-    
-    
-//     VStack(spacing: 0) {
-//
-//     Text("Total Hardness").font(.footnote).bold()
-//     if valueTH < 40 || valueTH > 750 {
-//     Text("\(Int(valueTH))").font(.title).bold().foregroundColor(Color.red)
-//     } else  {
-//     Text("\(Int(valueTH))").font(.title).bold()
-//     }
-//
-//     HStack {
-//     Text("\(Int(minTH))")
-//
-//     Slider(value: $valueTH, in: minTH...maxTH)
-//
-//     Text("\(Int(maxTH))")
-//     }.padding(EdgeInsets(top: 0, leading: 20, bottom: 20, trailing: 20))
-//     }
-//     VStack(spacing: 0) {
-//     Text("Total Chlorine").font(.footnote).bold()
-//
-//     if valueTC < 4 || valueTC > 15 {
-//     Text("\(Int(valueTC))").font(.title).bold().foregroundColor(Color.red)
-//     } else  {
-//     Text("\(Int(valueTC))").font(.title).bold()
-//     }
-//
-//     HStack {
-//     Text("\(Int(minTC))")
-//
-//     Slider(value: $valueTC, in: minTC...maxTC)
-//
-//
-//
-//     Text("\(Int(maxTC))")
-//     }.padding(EdgeInsets(top: 0, leading: 20, bottom: 20, trailing: 20))
-//     }
-//     VStack(spacing: 0) {
-//     Text("Free Chlorine").font(.footnote).bold()
-//
-//     if valueFC < 4 || valueFC > 15 {
-//     Text("\(Double(valueFC), specifier: "%.1f")").font(.title).bold().foregroundColor(Color.red)
-//     } else  {
-//     Text("\(Double(valueFC), specifier: "%.1f")").font(.title).bold()
-//     }
-//
-//     HStack {
-//     Text("\(Int(minFC))")
-//
-//     Slider(value: $valueFC, in: minFC...maxFC, step: 0.1)
-//
-//     Text("\(Int(maxFC))")
-//     }.padding(EdgeInsets(top: 0, leading: 20, bottom: 20, trailing: 20))
-//     }
-//     VStack(spacing: 0) {
-//     Text("pH").font(.footnote).bold()
-//
-//     if valuePH < 7.3 || valuePH > 8.0 {
-//     Text("\(Double(valuePH), specifier: "%.1f")").font(.title).bold().foregroundColor(Color.red)
-//     } else  {
-//     Text("\(Double(valuePH), specifier: "%.1f")").font(.title).bold()
-//     }
-//
-//     HStack {
-//     Text("\(Int(minPH))")
-//
-//     Slider(value: $valuePH, in: minPH...maxPH, step: 0.1)
-//
-//     Text("\(Int(maxPH))")
-//     }.padding(EdgeInsets(top: 0, leading: 20, bottom: 20, trailing: 20))
-//     }
-//     VStack(spacing: 0) {
-//
-//     Text("Total Alkalinity").font(.footnote).bold()
-//
-//     if valueTA < 40 || valueTA > 750 {
-//     Text("\(Int(valueTA))").font(.title).bold().foregroundColor(Color.red)
-//     } else  {
-//     Text("\(Int(valueTA))").font(.title).bold()
-//     }
-//
-//     HStack {
-//     Text("\(Int(minTA))")
-//
-//     Slider(value: $valueTA, in: minTA...maxTA)
-//
-//     Text("\(Int(maxTA))")
-//     }.padding(EdgeInsets(top: 0, leading: 20, bottom: 20, trailing: 20))
-//     }
-//     VStack(spacing: 0) {
-//     Text("Stabilizer (CYA)").font(.footnote).bold()
-//
-//     if valueST < 40 || valueST > 750 {
-//     Text("\(Int(valueST))").font(.title).bold().foregroundColor(Color.red)
-//     } else  {
-//     Text("\(Int(valueST))").font(.title).bold()
-//     }
-//
-//     HStack {
-//     Text("\(Int(minST))")
-//
-//     Slider(value: $valueST, in: minST...maxST)
-//
-//     Text("\(Int(maxST))")
-//     }.padding(EdgeInsets(top: 0, leading: 20, bottom: 20, trailing: 20))
-//     }  .navigationBarTitle(Text("Pool Water Test"), displayMode:.inline)
-//
-//        }
-//}
-
-//extension URLRequest {
-//    mutating func setBasicAuth(username: String, password: String) {
-//        let encodedAuthInfo = String(format: "%@:%@", username, password)
-//            .data(using: String.Encoding.utf8)!
-//            .base64EncodedString()
-//        addValue("Basic \(encodedAuthInfo)", forHTTPHeaderField: "Authorization")
-//        print("\(encodedAuthInfo)")
-//    }
-//}
 
 struct WaterTestView_Previews: PreviewProvider {
     static var previews: some View {
